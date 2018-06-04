6f93fc3c73e3c571e0fbd381bf89293c36fadcc7
karolgk <karol.kallajian@b4agroup.com>
2018-06-04 11:11:19 -0300

    aula 6

--- javascript-avancado-1/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-1/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -10,14 +10,22 @@ class NegociacaoController {
         
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
+
+		this._mensagem = new Mensagem();
+		this._mensagemView = new MensagemView($('#mensagemView'));
+		this._mensagemView.update(this._mensagem);
     }
     
     adiciona(event) {
         
-        event.preventDefault();
-        this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
-        this._limpaFormulario();   
+	    event.preventDefault();
+	    this._listaNegociacoes.adiciona(this._criaNegociacao());
+	    this._negociacoesView.update(this._listaNegociacoes);
+
+	    this._mensagem.texto = 'Negociacao adicionada com sucesso';
+	    this._mensagemView.update(this._mensagem);  
+
+	    this._limpaFormulario();
     }
     
     _criaNegociacao() {
